22359ef55d377220029beb0aaa33c76c21dc723f
prashant <prashant.aghara2111@gmail.com>
2022-11-19 16:13:18 +0530

    Pug Template Engine

--- node-template-engines/app.js
+++ node-template-engines/app.js
@@ -1,21 +1,30 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+//Handle bars
+app.engine("handlebars", expressHbs());
+app.set("view engine", "handlebars");
+app.set("views", "/views/handlebars");
+//Pug
+// app.set("view engine", "pug");
+// app.set("views", "views/pug");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use('/admin', adminData.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render("404", { title: "Not Found" });
 });
 
 app.listen(3000);
