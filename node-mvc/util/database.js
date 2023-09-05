804f46f482b2a559cb91caa45da3b67559f0eb37
prashant <prashant.aghara2111@gmail.com>
2023-09-05 18:40:14 +0530

    Remove Creds

--- node-mvc/util/database.js
+++ node-mvc/util/database.js
@@ -2,9 +2,7 @@ const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 let _db;
 const connectMongo = (callback) => {
-  MongoClient.connect(
-    "mongodb+srv://prashant:Svl0FdHyAGZ0dBdd@cluster0.eobih.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  MongoClient.connect("")
     .then((result) => {
       console.log("DB Connected");
       _db = result.db();
